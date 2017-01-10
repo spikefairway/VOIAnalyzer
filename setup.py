ee1a257af14479dcdf27641298c1d67ef1027aa1
spikefairway <k.matsubara91@gmail.com>
2017-01-10 15:40:14 +0900

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding : utf-8
+
+from distutils.core import setup
+
+setup(
+        name = "VOIAnalyzer",
+        packages = ["VOIAnalyzer"],
+        version = '0.1.0',
+        description = "Extract VOI statistics",
+        author = "Keisuke Matsubara",
+        author_email = "k.matsubara91@gmail.com",
+        url = "https://github.com/spikefairway/VOIAnalyzer.git",
+        classifiers = [
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: POSIX :: Linux",
+            "Development Status :: 2 - Pre-Alpha",
+            "Intended Audience :: Science/Research",
+            "Topic :: Scientific/Engineering :: Medical Science Apps."
+            ]
+    )
